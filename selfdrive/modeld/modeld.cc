e9c3c760500b74f337c46a0df6891ef1d225915a
hikee9123 <hikee9123@gmail.com>
2023-01-15 15:04:15 +0900

    fix

--- selfdrive/modeld/modeld.cc
+++ selfdrive/modeld/modeld.cc
@@ -83,7 +83,9 @@ mat3 update_calibration_(Eigen::Vector3d device_from_calib_euler, bool wide_came
 
   const auto cam_intrinsics = Eigen::Matrix<float, 3, 3, Eigen::RowMajor>(wide_camera ? ecam_intrinsic_matrix.v : fcam_intrinsic_matrix.v);
   Eigen::Vector3d  device_cal = euler2rot(device_from_calib_euler)
-  Eigen::Matrix<float, 3, 3, Eigen::RowMajor>  device_from_calib = device_cal.cast <float> ();
+
+  printf( device_cal  );
+  Eigen::Matrix<float, 3, 3, Eigen::RowMajor>  device_from_calib = cam_intrinsics;
 
 
   auto calib_from_model = bigmodel_frame ? calib_from_sbigmodel : calib_from_medmodel;
