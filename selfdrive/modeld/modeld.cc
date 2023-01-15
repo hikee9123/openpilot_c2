6a9a2a88fad818497d97dfe79582ff570ea4c934
hikee9123 <hikee9123@gmail.com>
2023-01-15 12:58:20 +0900

    fix

--- selfdrive/modeld/modeld.cc
+++ selfdrive/modeld/modeld.cc
@@ -8,6 +8,7 @@
 #include "cereal/messaging/messaging.h"
 #include "common/transformations/orientation.hpp"
 
+
 #include "cereal/visionipc/visionipc_client.h"
 #include "selfdrive/common/clutil.h"
 #include "selfdrive/common/params.h"
