c657ed758d67610d50f9c7efad6af8ecd8a002ec
hikee9123 <hikee9123@gmail.com>
2023-02-11 18:56:02 +0900

    hud

--- selfdrive/car/hyundai/navicontrol.py
+++ selfdrive/car/hyundai/navicontrol.py
@@ -304,7 +304,7 @@ class NaviControl():
 
 
   def update(self, c, CS, frame ):  
-    self.log_msg = ' {} {} {}  {}'.format( self.auto_brakePress_speed_set, self.wait_timer1, frame % 100, CS.cruise_set_speed_kph   )
+    self.log_msg = ' {} {} {}={}  {}'.format( self.auto_brakePress_speed_set, self.wait_timer1, CS.cruise_set_speed_kph, CS.out.cruiseState.speed, frame % 1000   )
     self.sm.update(0)
     # send scc to car if longcontrol enabled and SCC not on bus 0 or ont live
     btn_signal = None
