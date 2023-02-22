8b20344c6e03a039097150cbd47b4ea20dd74997
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2023-02-22 17:43:56 +0900

    Update paint.cc

--- selfdrive/ui/qt/atom/paint.cc
+++ selfdrive/ui/qt/atom/paint.cc
@@ -970,7 +970,7 @@ void OnPaint::ui_draw_debug1( QPainter &p )
   if ( scene->stopLine.used && scene->stopLine.x > 1.0 ) {
     text3.sprintf("Stop line:%.1f   X:%.1f", scene->stopLine.Prob, scene->stopLine.x );
     configFont( p, "Open Sans",  80, "Regular");
-    p.drawText( QRect(bb_x+300, 700, bb_w, 42), text3, textOpt );
+    p.drawText( QRect(bb_x+300, 700, bb_w, 80), text3, textOpt );
   }  
 
  // text3.sprintf("BF:%.1f   RL:%.1f°", scene->scr.accel_prob[0], scene->scr.accel_prob[1] );
