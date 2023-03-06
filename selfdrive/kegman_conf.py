6638faace5d5188ae0a3629899d463614d376d2a
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2023-03-06 18:38:22 +0900

    Update kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -2,13 +2,13 @@ import json
 import os
 
 
-json_file_name = '/data/atom_0761.json'
+json_file_name = '/data/openpilot/atom_0761.json'
 
 class kegman_conf():
   def __init__(self, CP=None):
     self.config = None
     self.init = { 
-        "sR_KPH": [30,60],  # Speed  kph
+        "sR_KPHV": [30,60],  # Speed  kph
         "sR_BPV": [[1.,30],     [1.,10]],  # steer
         "sR_steerRatioV": [[15.5,16.5],[15.5,16.5]],  # speed1[steer], speed2[steer]
         "lat_FACTORV": [[3.1,2.3],[2.7,2.4]],
